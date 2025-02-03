d38d60f4cda1a21ffe3caa339df001c82376fb31
Joaoaalves <joaoaa21@hotmail.com>
2025-02-03 11:47:43 -0300

    feat: add callback for video captions

---
+++ src/app/api/videos/callback/route.ts
@@ -0,0 +1,68 @@
+import { NextResponse } from 'next/server';
+import { createApiResponse, validateExternalRequest } from '@/lib/api';
+import { headers } from 'next/headers';
+import { prisma } from '@/lib/prisma';
+import { insertRedisData } from '@/lib/redis';
+
+const route = '/api/videos/callback';
+
+export default async function POST(request: Request) {
+    const headersList = await headers();
+    const body = await request.json();
+
+    try {
+        const valid = await validateExternalRequest(headersList);
+
+        if (!valid) {
+            return createApiResponse({
+                route,
+                body,
+                status: 401,
+                message: 'Not Authorized.',
+                error: 'Unauthorized request',
+            });
+        }
+
+        const { legenda, video_id } = body;
+
+        if (!legenda) {
+            return createApiResponse({
+                route,
+                body: body,
+                status: 405,
+                message: 'Dados incompletos.',
+                error: 'Missing legenda.',
+            });
+        }
+
+        const video = await prisma.video.update({
+            where: {
+                id: video_id,
+            },
+            data: {
+                legenda,
+            },
+        });
+
+        await insertRedisData({
+            userId: video.userId,
+            dataType: 'video',
+        });
+
+        return createApiResponse({
+            route,
+            body,
+            status: 200,
+            message: 'Video update successfull!',
+        });
+    } catch (error) {
+        const message = `${error instanceof Error ? error.message : error}`;
+        return createApiResponse({
+            route,
+            body,
+            status: 500,
+            message: 'Error while updating video data.',
+            error: message,
+        });
+    }
+}
